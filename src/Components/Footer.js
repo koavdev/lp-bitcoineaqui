84819b51ed5ba7b7a003bfe45eb882c28d326d84
koavdev <koavdev@gmail.com>
2023-06-20 22:45:47 -0300

    c18 - update footer on mobile layout

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <div className='footer-section-container'>
         <div className='footer-section-text-container'>
-            <h2 className='footer-text'>© 2023 Bitcoin é Aqui! Rolante/Riozinho. Todos os direitos reservados.</h2>
+            <h2 className='footer-text'>Bitcoin é Aqui © 2023 - Rolante/RS</h2>
+            <h2>Todos os direitos reservados</h2>
         </div>
     </div>
   )
