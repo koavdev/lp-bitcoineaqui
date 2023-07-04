010a5dd06377467ee16c58ce5f032665c30764e6
koavdev <koavdev@gmail.com>
2023-07-04 16:31:08 -0300

    c46 - create table and import comercio data

--- src/Pages/comercio.js
+++ src/Pages/comercio.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import DataTable from 'react-data-table-component'
 
 const comercio = () => {
+  const columns = [
+    {
+      name: 'Tipo',
+      selector: row => row.tipo
+    },
+    {
+      name: 'Estabelecimento',
+      selector: row => row.nome,
+      sortable: true
+    },
+    {
+      name: 'Telefone',
+      selector: row => row.telefone,
+      sortable: true
+    },
+    {
+      name: 'Instagram',
+      selector: row => row.instagram,
+      sortable: true
+    }
+  ];
+
+  const data = require('../Utils/Pontos.json')
+
   return (
     <div>
         <Navbar/>
         <div className='page-container' style={{border: '1px solid red'}}>
-        <h1>comercio</h1>
+          <div className='comercio-table'>
+            <DataTable columns={columns} data={data}>
+
+            </DataTable>
+            </div>
         </div>
         <Footer/>
     </div>
